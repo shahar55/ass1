d019d57ee60de2ea7873975b89ad5c81a7029d40
shahar55 <sasa0192837465@gmail.com>
2021-07-31 18:11:40 +0300

    create unclassified class cpp and hpp

---
+++ unclassifiedFlower.cpp
@@ -0,0 +1,2 @@
+#include "unclassifiedFlower.hpp"
+unclassifiedFlower::unclassifiedFlower(int x1,int x2,int x3,int x4):Flower(x1,x2,x3,x4){}
---
+++ unclassifiedFlower.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+#include "flower.hpp"
+class unclassifiedFlower:public Flower{
+    public:
+        unclassifiedFlower(int x1,int x2,int x3,int x4); // constructor
+};

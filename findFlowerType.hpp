8aa97e6202a4315ca7a5df1def130b1f05d3711f
shahar55 <sasa0192837465@gmail.com>
2021-08-01 14:57:59 +0300

    add file that find the kind a unclass flower

---
+++ findFlowerType.hpp
@@ -0,0 +1,9 @@
+#include "classifiedFlower.hpp"
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include "csvReader.hpp"
+#include "unclassifiedFlower.hpp"
+std::string findFlowerName(UnclassifiedFlower test, std :: vector<ClassifiedFlower> data);

8d449bd1c24d897b09b52b9ccd1954626aa0e952
idob10 <barkaido21@gmail.com>
2021-08-09 19:28:42 +0300

    Deleted unnecesarry files

--- createClassedFlowers.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <fstream>
-#include <vector>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include "classifiedFlower.hpp"
-std::vector<ClassifiedFlower> createClassedFlowersFromVector (std::vector<std::vector<std::string>>& data);
--- createunClassedFlowers.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <fstream>
-#include <vector>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include "unclassifiedFlower.hpp"
-std::vector<UnclassifiedFlower> createUnClassedFlowersFromVector (std::vector<std::vector<std::string>>& data);
